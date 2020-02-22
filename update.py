e96b6d942f0fbbb86e5aabc49470cfa6df1860b3
Mike4driver <leahcim12000@gmail.com>
2020-02-22 15:52:17 -0800

    added some images and made update.py use multiple threads to speed up link grabbing

--- update.py
+++ update.py
@@ -5,28 +5,33 @@ import writeAllToJson as nr
 import time
 import sqlite3
 import datetime
+import concurrent.futures
 
 if __name__=="__main__":
-    conn = sqlite3.connect("novels.db")
+    start_time = time.time()
+    conn = sqlite3.connect("novels.db", check_same_thread=False)
     curs = conn.cursor()
     novelLinks = [row[0] for row in curs.execute("SELECT link from links")]
     
     # novelLinks = [link.replace("\n", "") for link in novelLinks]
-    os.environ["LANG"] = "en_US.UTF-8"
-    chrome_options = Options()
-    chrome_options.add_experimental_option("prefs", {'profile.managed_default_content_settings.javascript':2})
-    # chrome_options.add_extension("./Driver/uBlock.crx")
-    browser = webdriver.Chrome(executable_path=r'Driver\chromedriver.exe', chrome_options=chrome_options)
+    # os.environ["LANG"] = "en_US.UTF-8"
+    # chrome_options = Options()
+    # chrome_options.add_experimental_option("prefs", {'profile.managed_default_content_settings.javascript':2})
+    # # chrome_options.add_extension("./Driver/uBlock.crx")
+    # browser = webdriver.Chrome(executable_path=r'Driver\chromedriver.exe', chrome_options=chrome_options)
 
-    for link in novelLinks:
-        curs.execute("UPDATE links SET lastUpdated=? WHERE link=?", [datetime.datetime.now(), link])
-        nr.getNovelOnDemand(link, browser, conn, curs)
+    with concurrent.futures.ProcessPoolExecutor(max_workers=10) as executer:
+        futureToNovel = {executer.submit( nr.getNovelOnDemand, novelLink, conn, curs):novelLink for novelLink in novelLinks}
 
+    # for link in novelLinks:
+    #     nr.getNovelOnDemand(link, conn, curs)
+
+        # curs.execute("UPDATE links SET lastUpdated=? WHERE link=?", [datetime.datetime.now(), link])
     conn.commit()
     curs.close()    
     conn.close()
 
-    browser.quit()
-
+    # browser.quit()
+    print("--------%s seconds---------" % (time.time() -start_time))
 
     
--- writeAllToJson.py
+++ writeAllToJson.py
@@ -9,7 +9,7 @@ import time
 import sqlite3
 import datetime
 from multiprocessing import Pool
-
+from concurrent.futures import ThreadPoolExecutor
 
 def getAllChapterLinks(link, browser):
     """
@@ -57,76 +57,6 @@ def getChapterTexts(link, browser):
         return [chapterLinkNumber, chapterText]
     except:
         return False
-
-def getNovelLinks():
-    with open("theLinks.txt", "r", encoding="utf8") as f:
-        links = [line[:-2] for line in f.readlines()]
-    return links
-
-def readNovelJson():
-    with open('novels.json', 'r') as f:
-        novelJson = json.load(f)
-    return novelJson
-
-
-def writeNewJson(browser):
-
-    links = getNovelLinks()
-
-    novelJson = {
-        "Novels": []
-    }
-    i = 0
-    for link in links:
-        i+=1
-        print('{}/{} Novels Completed!'.format(i, len(links)))
-        try:
-            novelJson["Novels"].append(getAllChapterLinks(link, browser))
-            with open("novels.json", "w") as f:
-                json.dump(novelJson, f)
-        except:
-            pass
-        
-        # print(novelJson)
-
-def getChapterLinksforCompare():
-    chapterList = []
-    for novel in readNovelJson()["Novels"]:
-        for chapter in novel["Chapters"]:
-            chapterList.append(chapter["chapterLink"])
-    return chapterList
-
-def updateJson():
-    links = getNovelLinks()
-    novelJson = readNovelJson()
-    chapterList = getChapterLinksforCompare()
-
-    for link in links:
-        for chapter in chapterList:
-            if link in chapter:
-                links.remove(link)
-                break
-    return links
-
-def writeNewNovel(searchTerm, browser, novelJson):
-    speaker = Sapi()
-    speaker.set_rate(2)
-    voices = speaker.get_voices()
-    speaker.set_voice(voices[1])
-
-
-    for novel in novelJson["Novels"]:
-        if 'Name' in novel:
-            if novel["Name"] == searchTerm:
-                newPath = r"Novels\{}".format(novel["Name"].replace("?", ''))
-                if not os.path.exists(newPath):
-                    os.makedirs(newPath)
-                for chapter in novel["Chapters"]:
-                    chapterLinkNumber, chapterText = getChapterTexts(chapter['chapterLink'], browser)
-                    if not os.path.isfile(newPath + f"\{chapterLinkNumber}.mp3"):
-                        if chapterText:
-                            speaker.create_recording(newPath + f"\{chapterLinkNumber}.mp3", re.sub(r'Chapter \w+', r'', chapterText))
-                            print(f"Chapter {chapter['chapterNumber']}/{len(novel['Chapters'])} finished")
 
 def stringIsNum(x):
     try:
@@ -217,7 +147,12 @@ def chaptersToAudio(chapter, novel):
         conn.close()
             
 
-def getNovelOnDemand(novelLink, browser, conn, curs):
+def getNovelOnDemand(novelLink, conn, curs):
+    os.environ["LANG"] = "en_US.UTF-8"
+    chrome_options = Options()
+    chrome_options.add_experimental_option("prefs", {'profile.managed_default_content_settings.javascript':2})
+    # chrome_options.add_extension("./Driver/uBlock.crx")
+    browser = webdriver.Chrome(executable_path=r'Driver\chromedriver.exe', chrome_options=chrome_options)
     novel = getAllChapterLinks(novelLink, browser)
     novel["Chapters"] = [chapter for chapter in novel["Chapters"] if "collapse" not in chapter["chapterLink"]]
     newPath = r"Novels\{}".format(novel["Name"].replace("?", ''))
@@ -235,11 +170,9 @@ def getNovelOnDemand(novelLink, browser, conn, curs):
         if newChapter:
             nonPresentChapters.append((newChapter))
     
-
-        
     print(f"System has {os.cpu_count()} cores... creating {os.cpu_count()} threads ") if len(nonPresentChapters) >= os.cpu_count() else print (f"Creating {len(nonPresentChapters)} threads") 
     # This will prevent the behavior we currently see where a the latest books in the update are all only being handled by the last process
-    with Pool(processes=12) as p:
+    with Pool(processes=os.cpu_count()) as p:
         # for chapter in novel["Chapters"]:
         p.starmap(chaptersToAudio, [(chapter, novel) for chapter in nonPresentChapters])
         
@@ -258,18 +191,10 @@ if __name__ == '__main__':
     if newNovelLink not in oldNovelLinks:
         curs.execute("INSERT INTO links VALUES (?, ?, ?, ?)", [newNovelLink, datetime.datetime.now(), None, None])
         conn.commit()
-    
 
-    os.environ["LANG"] = "en_US.UTF-8"
-    chrome_options = Options()
-    chrome_options.add_experimental_option("prefs", {'profile.managed_default_content_settings.javascript':2})
-    # chrome_options.add_extension('./Driver/uBlock-Origin-Internetový-obchod-Chrome_v1.23.0.crx')
-    # chrome_options.add_extension('./Driver/uBlock-Origin.crx')
-    browser = webdriver.Chrome(executable_path=r'Driver\chromedriver.exe', chrome_options=chrome_options)
-    getNovelOnDemand(newNovelLink, browser, conn, curs)
+    getNovelOnDemand(newNovelLink, conn, curs)
     conn.close()
 
 
 
-    browser.quit()
     print("--------%s seconds---------" % (time.time() -start_time))
