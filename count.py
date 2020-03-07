29f17267dc88fa56c44f19d5ba1eb64b35364a53
Mike4driver <leahcim12000@gmail.com>
2020-03-07 13:14:35 -0800

    made county.py take a 10th of the time and memory with some optimizations

--- count.py
+++ count.py
@@ -1,11 +1,17 @@
 import sqlite3
+import time
 
 if __name__ == "__main__":
     conn = sqlite3.connect("novels.db")
-    curs = conn.cursor()
 
-    text = ''
-    for row in conn.execute(f"SELECT [text] FROM chapters WHERE novelLink LIKE '%asura%' ORDER BY chapterNumber"):
-        text += " " + row[0].strip()
+    start_time = time.time()
+    textlist = (" " + row[0].strip() for row in conn.execute(f"SELECT [text] FROM chapters WHERE novelLink LIKE '%asura%'"))
 
-    print(len(text.split(" ")))
+    total = 1
+    for text in textlist:
+        for letter in text:
+            if(letter == ' ' or letter == '\n' or letter == '\t'):
+                total = total + 1
+
+    print(total)
+    print("--------%s seconds---------" % (time.time() -start_time))
